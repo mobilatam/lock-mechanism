4d39fba52887c30d769d4d3c6fa8a402e0928696
Leonardo Achá Boiano <leonardoacha@hotmail.com>
2024-07-25 11:36:42 -0400

    Update twai_utils.c
    TWAI utility functions updated to follow the OMNI CAN PROTOCOL specifications

--- main/twai_utils/twai_utils.c
+++ main/twai_utils/twai_utils.c
@@ -36,31 +36,64 @@ esp_err_t configure_twai(void) {
 // Función para manejar los mensajes recibidos
 void handle_twai_message(twai_message_t message) {
     // Verifica el comando recibido y mueve el servomotor en consecuencia
-    switch (message.data[0]) {
-        case 0x01: //OPEN BL
-            if (~gpio_get_level(GPIO_BATTERY_LOCK_CLOSE))
-            {
-                unlock_battery(); 
-            }
-            break;
-        case 0x02: //OPEN DL
-            if (~gpio_get_level(GPIO_DOOR_LOCK_CLOSE))
-            {
-                unlock_door(); 
-            }
-            break;
-        case 0x03: //CLOSE BL
-            if (~gpio_get_level(GPIO_BATTERY_LOCK_OPEN))
-            {
-                lock_battery(); 
-            }
-            break;
-        case 0x04: //CLOSE DL
-            if (~gpio_get_level(GPIO_DOOR_LOCK_OPEN))
-            {
-                lock_door();
-            }
-            break;
+    ESP_LOGW(TWAI_TAG, "CAN ID: 0x%X", message.identifier);
+    switch (message.identifier) {
+        case (0x10 << 24) | (0x16 << 19) | (0x00 << 16) | (0x04 << 8) | 0x00:
+            // Process the first byte of the message data to unlock/lock the battery
+            if (message.data_length_code == 8) {
+                uint8_t command = message.data[0];
+                
+                switch (command) {
+                    case 0x00: // Unlock
+                        ESP_LOGI(TWAI_TAG, "Received Unlock command");
+                        //OPEN BL
+                        if (~gpio_get_level(GPIO_BATTERY_LOCK_CLOSE)){
+                            unlock_battery(); 
+                            send_open_status(0x16);
+                        } 
+                        break;
+                    case 0x01: // Lock
+                        ESP_LOGI(TWAI_TAG, "Received Lock command");
+                        //CLOSE BL
+                        if (~gpio_get_level(GPIO_BATTERY_LOCK_OPEN)){
+                            lock_battery(); 
+                            send_close_status(0x16);
+                        }
+                        break;
+                    default:
+                        ESP_LOGW(TWAI_TAG, "Unknown command received: 0x%X", command);
+                        break;
+                } else {
+                ESP_LOGW(TWAI_TAG, "Unexpected data length: %d", message.data_length_code);
+                }
+        case (0x10 << 24) | (0x18 << 19) | (0x00 << 16) | (0x04 << 8) | 0x00: 
+            // Process the first byte of the message data to unlock/lock the battery
+            if (message.data_length_code == 8) {
+                uint8_t command = message.data[0];
+                
+                switch (command) {
+                    case 0x00: // Unlock
+                        ESP_LOGI(TWAI_TAG, "Received Unlock command");
+                        //OPEN DL
+                        if (~gpio_get_level(GPIO_DOOR_LOCK_CLOSE)){
+                            unlock_door();
+                            send_open_status(0x18);
+                        } 
+                        break;
+                    case 0x01: // Lock
+                        ESP_LOGI(TWAI_TAG, "Received Lock command");
+                        //CLOSE DL
+                        if (~gpio_get_level(GPIO_DOOR_LOCK_OPEN)){
+                            lock_door(); 
+                            send_close_status(0x18);
+                        }
+                        break;
+                    default:
+                        ESP_LOGW(TWAI_TAG, "Unknown command received: 0x%X", command);
+                        break;
+                } else {
+                ESP_LOGW(TWAI_TAG, "Unexpected data length: %d", message.data_length_code);
+                }
         default:
             ESP_LOGW(TWAI_TAG, "Unknown command received: 0x%X", message.data[0]);
             break;
@@ -68,20 +101,36 @@ void handle_twai_message(twai_message_t message) {
 }
 
 // Función para enviar un mensaje TWAI (CAN)
-void send_twai_message(uint8_t command) {
-    // Crea un mensaje TWAI
+void send_twai_message(uint32_t identifier, uint8_t status) {
+    // Create a TWAI message
     twai_message_t message;
-    message.identifier = 0x101; // ID del mensaje
-    message.extd = 0; // Usa formato de ID estándar
-    message.data_length_code = 1; // Longitud de los datos del mensaje
-    message.data[0] = command; // Dato del mensaje
+    message.identifier = identifier; // Message ID
+    message.extd = 0; // Standard format (11-bit identifier)
+    message.rtr = 0; // Data frame
+    message.data_length_code = 8; // DLC - length of data
+    message.data[0] = 0x01; // Firmware version MSB
+    message.data[1] = 0x02; // Firmware version LSB
+    message.data[2] = status; // Status code (locked/unlocked)
+    // Reserve (remaining bytes)
 
-    // Envía el mensaje TWAI
+    // Transmit the TWAI message
     if (twai_transmit(&message, pdMS_TO_TICKS(1000)) == ESP_OK) {
-        ESP_LOGI(TWAI_TAG, "Message transmitted: 0x%X", command);
+        ESP_LOGI(TWAI_TAG, "Message transmitted: Status: 0x%02X", status);
     } else {
         ESP_LOGE(TWAI_TAG, "Failed to transmit message");
     }
+}
+
+// Function to send 'open' status
+void send_open_status(uint8_t src) {
+    uint32_t can_id = (src << 24) | (0x10 << 19) | (0x2 << 16) | (0x04 << 8) | 0x01;;
+    send_twai_message(can_id, 0x01); // 0x01 represents 'unlocked'
+}
+
+// Function to send 'closed' status
+void send_closed_status(uint8_t src) {
+    uint32_t can_id = (src << 24) | (0x10 << 19) | (0x2 << 16) | (0x04 << 8) | 0x01;;
+    send_twai_message(can_id, 0x00); // 0x00 represents 'locked'
 }
 
 /********************************* END OF FILE ********************************/
